47be602b302433e4c61419e781683512da16f8f4
akkva <150437642+akkva@users.noreply.github.com>
2024-03-08 13:56:19 +0800

    Add files via upload

---
+++ userChromeJS/profile/chrome/JS/move-buttons.uc.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name     Move unmovable
+// @description     Just moves unmovable buttons
+// ==/UserScript==
+
+(function () {
+    function init() {
+	let toolbar = document.getElementById("TabsToolbar");
+	let back = document.getElementById("back-button");
+	let forward = document.getElementById("forward-button");
+	let reload = document.getElementById("reload-button");
+        let stop = document.getElementById("stop-button");
+    toolbar._customizationTarget.prepend(back);
+    back.after(forward);
+	toolbar._customizationTarget.appendChild(reload);
+        toolbar._customizationTarget.appendChild(stop);
+    }
+	_ucUtils.windowIsReady(window)
+	.then(()=>{
+	  init();
+	});
+})();
