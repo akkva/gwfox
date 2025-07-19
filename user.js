87178a8d893d15579f43bef0de435f3c087115f6
soulhotel <155501797+soulhotel@users.noreply.github.com>
2025-07-19 14:23:09 -0400

    Documentation PR

---
+++ user.js
@@ -0,0 +1,18 @@
+/* gwfox https://github.com/akkva/gwfox */
+
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("svg.context-properties.content.enabled", true);
+user_pref("sidebar.animation.enabled", false);
+
+user_pref("widget.transparent-windows", true);
+user_pref("widget.windows.mica", true);
+user_pref("widget.windows.mica.extra", true);
+user_pref("widget.windows.mica.popups", 2);
+user_pref("widget.windows.mica.toplevel-backdrop", 2);
+user_pref("widget.macos.titlebar-blend-mode.behind-window", true);
+user_pref("browser.tabs.allow_transparent_browser", true);
+
+/* (可选项 | OPTIONAL)
+user_pref("gwfox.plus", true);
+user_pref("widget.macos.native-context-menus", true);
+*/
